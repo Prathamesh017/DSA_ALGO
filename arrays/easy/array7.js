a883b4968a9090da3fe4ecd8674f85198e94e988
prathamesh thakare <prathamesh06pt@gmail.com>
2024-03-06 13:15:10 +0530

    feat: easy prblm done

--- arrays/easy/array7.js
+++ arrays/easy/array7.js
@@ -17,4 +17,51 @@ function longestSubArr(arr,k){
   return max;
 
 }
-console.log(longestSubArr([1,2,3,1,1,1,1],3))
+//BY HASHING(see)
+function longestSubArrHash(arr,k){
+  let max=0;
+  let sum=0;
+  let map=new Map();
+  for (let i = 0; i < arr.length; i++) {
+    sum=sum+arr[i];
+    //if the value already exist don't update the map
+    if(!map.has(sum)){
+      map.set(sum,i);
+    }
+    if(sum===k){
+      max=max>map.get(i)?max:map.get(i);
+
+    }
+    let rem=sum-k
+     if (map.has(rem)){
+      let ans=i-(map.get(rem));
+      max=max>ans?max:ans;
+    }
+  }
+  return max;
+}
+
+//BEST APPROACH WITH 2 pointer
+function longestSubArrPointer(arr,k){
+  let left=0;
+  let max=0;
+  let sum=0;
+  let right=0;
+
+  while(right<arr.length){
+    sum=sum+arr[right];
+    if(sum>k){
+      sum=sum-arr[left];
+      left++
+    }
+    if(sum==k){
+      max=max>right-left+1?max:right-left+1;
+    }
+    right++;
+  }
+  return max;
+  
+}
+console.log(longestSubArrPointer([1,2,3,1,1,1,1],3))
+// console.log(longestSubArrPointer([1,2,1,3],4))
+// console.log(longestSubArrHash([2,0,0,3],3))
